8670fdf4243dab10b0b8e020c4662cce14a804dc
md_5 <git@md-5.net>
2017-05-15 15:16:15 +1000

    Skeleton API Implementations

--- src/main/java/org/bukkit/entity/Arrow.java
+++ src/main/java/org/bukkit/entity/Arrow.java
@@ -71,4 +71,14 @@ public interface Arrow extends Projectile {
          */
         CREATIVE_ONLY
     }
+
+    // Spigot start
+    public class Spigot extends Entity.Spigot
+    {
+
+    }
+
+    @Override
+    Spigot spigot();
+    // Spigot end
 }
