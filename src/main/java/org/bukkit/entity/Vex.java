62b1a146cbedba84fd1057d1bee2a6cadf76ca59
Aikar <aikar@aikar.co>
2018-07-04 15:29:21 -0400

    Vex#getSummoner API
    Get's the Mob that summoned this Vex

--- src/main/java/org/bukkit/entity/Vex.java
+++ src/main/java/org/bukkit/entity/Vex.java
@@ -3,4 +3,10 @@ package org.bukkit.entity;
 /**
  * Represents a Vex.
  */
-public interface Vex extends Monster { }
+public interface Vex extends Monster {
+    /**
+     * @return What Entity (most likely an Evoker, but not guaranteed) summoned this Vex
+     */
+    Mob getSummoner(); // Paper
+
+}
