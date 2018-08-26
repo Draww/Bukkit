b2ac5fb0f21447d5dc1c6abfea8aeee28c3f3c48
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-08-25 19:56:42 -0500

    Add PhantomPreSpawnEvent

--- src/main/java/org/bukkit/entity/Phantom.java
+++ src/main/java/org/bukkit/entity/Phantom.java
@@ -14,4 +14,13 @@ public interface Phantom extends Flying {
      * @param sz The new size of the phantom.
      */
     public void setSize(int sz);
+
+    // Paper start
+    /**
+     * Get the UUID of the entity that caused this phantom to spawn
+     *
+     * @return UUID
+     */
+    public java.util.UUID getSpawningEntity();
+    // Paper end
 }
